1d5c61c4e0d2fe08e0425ce799f280a8aa5778d7
Gabriela Gonzalez <gabytasmile7@gmail.com>
2020-11-19 18:10:07 -0300

    se crea componente nav, cart y tab

---
+++ src/components/Cart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div><v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-cart</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-app-bar color="teal lighten-3" dense dark height="100">
+        <v-toolbar-title>MyPet</v-toolbar-title>
+        <v-spacer></v-spacer>
+
+       <v-col cols="12" sm="6" md="3">
+          <v-text-field
+          class="mt-6"
+            label="Bucador de producto o categoría"
+            outlined
+            append-icon="mdi-magnify"
+          ></v-text-field>
+        </v-col>
+      <cart></cart>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+import Cart from '@/components/Cart'
+  export default {
+    components: {
+      Cart
+    }
+  }
+</script>
